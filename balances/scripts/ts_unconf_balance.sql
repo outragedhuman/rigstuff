f007f5e9e7617d039cab694d6422ff0ac4092501
Nathan <thekashkings@gmail.com>
2016-11-28 18:07:23 -0500

    Add difficulty to the database, polling this at the same time as the unconfirmed balance to provide tracking at the same time. Fixes #5

--- balances/scripts/ts_unconf_balance.sql
+++ balances/scripts/ts_unconf_balance.sql
@@ -3,5 +3,6 @@ CREATE TABLE balances
   id              INT unsigned NOT NULL AUTO_INCREMENT, # Unique ID for the record
   time            TIMESTAMP NOT NULL,                   # Time of entry
   balance         FLOAT unsigned NOT NULL,
+  difficulty      INT unsigned NOT NULL,                # Block Diff, taken at same time as balance
   PRIMARY KEY     (id)                                  # Make the id the primary key
 );
--- balances/unconf_balance.sh
+++ balances/unconf_balance.sh
@@ -6,13 +6,15 @@ timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
 
 suffix=' zec'
 api_url='https://zec.suprnova.cc/index.php?page=api&action=getuserbalance&api_key='$api_key'&id=45019'
+explorer_url='https://api.zcha.in/v1/mainnet/network'
 
 #Grab balance from Suprnovapool API, extract unconfirmed balance only
 unconf_balance="$(curl -X GET $api_url | jq '.getuserbalance.data.unconfirmed')"
+diff="$(curl -X GET $explorer_url | jq '.difficulty')"
 
 #Legacy logging (deprecated, will remove soon)
 #echo "$timestamp $unconf_balance$suffix" >> $log_location
 
 #Insert balance into database (Note: 'login-path' must be configured ahead of time using "mysql_config_editor set --login-path=local --host=localhost --user=username --password")
-mysql --login-path=local -e"use zec_mining; INSERT INTO balances (balance, time) VALUES
-  ( '$unconf_balance', '$timestamp' );"
+mysql --login-path=local -e"use zec_mining; INSERT INTO balances (balance, time, difficulty) VALUES
+  ( '$unconf_balance', '$timestamp', '$diff' );"
