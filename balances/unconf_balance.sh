bf1f053fb7432996247b357c9560cbab220f278f
Nathan <thekashkings@gmail.com>
2016-11-27 22:35:05 -0500

    Increase readability of API query, additional comments added referencing #1

--- balances/unconf_balance.sh
+++ balances/unconf_balance.sh
@@ -1,12 +1,16 @@
 #Set up some variables for logging
 timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
+
 #Legacy log file write location (will be deprecated and replaced with DB logging eventually)
 log_location='/home/ubiq/unconf_balance.log'
-#Currency suffix
-suffix=' zec'
 
-#Grab balance from Suprnovapool API
-unconf_balance="$(curl -X GET "https://zec.suprnova.cc/index.php?page=api&action=getuserbalance&api_key="$api_key"&id=45019" | jq ".getuserbalance.data.unconfirmed")"
+suffix=' zec'
+api_url='https://zec.suprnova.cc/index.php?page=api&action=getuserbalance&api_key="$api_key"&id=45019'
+
+#Grab balance from Suprnovapool API, extract unconfirmed balance only
+unconf_balance="$(curl -X GET '$api_url' | jq '.getuserbalance.data.unconfirmed')"
+
+#Legacy logging
 echo "$timestamp $unconf_balance$suffix" >> $log_location
 
 #Insert balance into database (Note: 'login-path' must be configured ahead of time using "mysql_config_editor set --login-path=local --host=localhost --user=username --password")
