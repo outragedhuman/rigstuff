c27bcfc7907b95257ebedbebb6bb0b521962b4e6
Nathan <thekashkings@gmail.com>
2016-11-27 22:23:06 -0500

    Adding comments to unconf_balance script, referencing #1. Still more documentation needed before complete.

--- balances/unconf_balance.sh
+++ balances/unconf_balance.sh
@@ -1,8 +1,14 @@
-timestamp="$(date '+%H:%M:%S') "
+#Set up some variables for logging
+timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
+#Legacy log file write location (will be deprecated and replaced with DB logging eventually)
 log_location='/home/ubiq/unconf_balance.log'
+#Currency suffix
 suffix=' zec'
 
-echo $api_key
-
+#Grab balance from Suprnovapool API
 unconf_balance="$(curl -X GET "https://zec.suprnova.cc/index.php?page=api&action=getuserbalance&api_key="$api_key"&id=45019" | jq ".getuserbalance.data.unconfirmed")"
-echo $timestamp$unconf_balance$suffix >> $log_location
+echo "$timestamp $unconf_balance$suffix" >> $log_location
+
+#Insert balance into database (Note: 'login-path' must be configured ahead of time using "mysql_config_editor set --login-path=local --host=localhost --user=username --password")
+mysql --login-path=local -e"use zec_mining; INSERT INTO balances (balance, time) VALUES
+  ( '$unconf_balance', '$timestamp' );"
