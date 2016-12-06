22b6875ab3ed0b2ed8a12fc0b7d2f2e517b8127a
Nathan <thekashkings@gmail.com>
2016-12-06 14:21:26 -0500

    Ported poolstats.sh to poolstats.py in preparation for larger database operation overhauls related to #10. Minor cleanup to a couple of files.

---
+++ poolstats/poolstats.py
@@ -0,0 +1,56 @@
+import datetime
+import requests
+import config
+import simplemysql
+
+#Timestamp for logging
+timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+print timestamp
+
+explorer_url='https://api.zcha.in/v1/mainnet/network'
+
+pool_api_key= config.pool_api_key
+pool_url='https://zec.suprnova.cc/index.php?page=api&action=getuserbalance&api_key=' + pool_api_key + '&id=45019'
+
+#Function for making an API call
+def make_request(url):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise ApiError('url {}'.format(resp.status_code))
+    return resp
+
+#Query network information from block explorer
+resp = make_request(explorer_url)
+
+#Pull out just the difficulty
+diff = resp.json()['difficulty']
+
+#Query pool for statistics
+resp = make_request(pool_url)
+
+#Pull out just the unconfirmed balance
+unconf_balance = resp.json()['getuserbalance']['data']['unconfirmed']
+
+print '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+
+try:
+    #Make database connection using parameters from config file
+    db = simplemysql.SimpleMysql(
+        host=config.host,
+        db=config.db,
+        user=config.user,
+        passwd=config.passwd
+    )
+except:
+    print ("Unable to connect to the database, did you set up your config.py file?")
+    raise
+
+try:
+
+    #Insert values into db
+    db.insert("balances", {"balance": unconf_balance, "time": timestamp, "difficulty": diff} )
+    db.commit()
+
+except:
+    print ("Error inserting values into db")
+
