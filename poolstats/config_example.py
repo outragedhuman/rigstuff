5a17d89e26aa75d64226b57c7db0b6d802e420c6
Nathan <thekashkings@gmail.com>
2016-12-06 14:33:28 -0500

    Updating config example with comments and additional parameters.

--- poolstats/config_example.py
+++ poolstats/config_example.py
@@ -1,1 +1,6 @@
-pool_api_key = '' 
+pool_api_key = '' #Any MPOS pool api key should work 
+host=''           #Host where the DB resides, 127.0.0.1 if the same server this script is running on
+db=''             #Database name
+user=''           #User to connect to the DB with
+passwd=''         #Passwd for this user
+
