c581a1579b1d7e5a541e3e4ab0fa8145561a5854
outragedhuman <thekashkings@gmail.com>
2016-12-20 20:27:37 -0500

    Cleaned up some missing dependencies and installation notes for pool stats utility, corrected the script to run by the cron

--- poolstats/scripts/makecron.sh
+++ poolstats/scripts/makecron.sh
@@ -3,5 +3,5 @@ crontab -e
 #Sets up cron variable for api key
 (crontab -l ; echo "api_key='Your_MPOS_API_Key_Here'") | crontab - –
 #Sets up cron entry for script to scrape balance data
-(crontab -l ; echo "*/2 * * * * /home/ubiq/.profile; /home/ubiq/rigstuff/balances/unconf_balance.sh") | crontab - –
+(crontab -l ; echo "*/2 * * * * python /home/ubiq/rigstuff/balances/poolstats.py") | crontab - –
 
